5177b14b4bafe15ae5c5ec8107398c141100e7ad
Corum Gold <goldcorum@gmail.com>
2022-10-05 13:41:17 +0200

    Set up browser routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            {/* Nested Routes */}
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
