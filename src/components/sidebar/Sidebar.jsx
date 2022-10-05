aaf226e4661bc170870338e1becd7a50e71a548d
Corum Gold <goldcorum@gmail.com>
2022-10-05 14:40:52 +0200

    Add icons to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,18 @@
-import React from "react";
-import "./sidebar.scss"
-
+import "./sidebar.scss";
+import {
+  DashboardOutlined,
+  SettingsSystemDaydreamOutlined,
+  PsychologyOutlined,
+  SettingsApplicationsOutlined,
+  AccountCircleOutlined,
+  ExitToAppOutlined,
+  NotificationsNoneOutlined,
+  InsertChartOutlined,
+  LocalShippingOutlined,
+  CreditCardOutlined,
+  StoreOutlined,
+  PersonOutlined
+} from "@mui/icons-material";
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -10,17 +22,59 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardOutlined className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+          <PersonOutlined className="icon" />
+
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+          <StoreOutlined className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+          <CreditCardOutlined className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+          <LocalShippingOutlined className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+          <InsertChartOutlined className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+          <NotificationsNoneOutlined className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlined className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlined className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+          <SettingsApplicationsOutlined className="icon" />
+            <span>Setting</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+          <AccountCircleOutlined className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+          <ExitToAppOutlined className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
