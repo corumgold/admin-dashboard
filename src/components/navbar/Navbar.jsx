4d4860327b8113f7f61560892e424d8ca8f8f530
Corum Gold <goldcorum@gmail.com>
2022-10-05 17:54:50 +0200

    Style navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import "./navbar.scss";
+import {
+  SearchOutlined,
+  LanguageOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  ListOutlined,
+} from "@mui/icons-material";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined className="icon" />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/11293719/pexels-photo-11293719.jpeg?cs=srgb&dl=pexels-vietnam-photographer-11293719.jpg&fm=jpg"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
